ce9ef061aab1ccfa336b33835f3c1bce00b587d8
MehranMoalemi <mohammadmoallemi78@gmail.com>
2023-04-01 04:05:20 +0430

    MuiSpeedDial comments

--- react-mui-demo/src/components/MuiSpeedDial.tsx
+++ react-mui-demo/src/components/MuiSpeedDial.tsx
@@ -9,8 +9,18 @@ export const MuiSpeedDial = () => {
     <SpeedDial
       ariaLabel='Navigation speed dial'
       sx={{ position: 'absolute', bottom: 16, right: 16 }}
-      icon={<SpeedDialIcon openIcon={<EditIcon />} />}>
-      <SpeedDialAction icon={<CopyIcon />} tooltipTitle='Copy' tooltipOpen />
+      icon={
+        <SpeedDialIcon
+          //this icon will show a pen instead of plus after clicking
+        openIcon={<EditIcon />}
+      />}
+    >
+      <SpeedDialAction icon={<CopyIcon />}
+        //describe the icon on hover or long press in mobile
+        tooltipTitle='Copy'
+        //this will make the tooltip always shown
+        tooltipOpen
+      />
       <SpeedDialAction icon={<PrintIcon />} tooltipTitle='Print' tooltipOpen />
       <SpeedDialAction icon={<ShareIcon />} tooltipTitle='Share' tooltipOpen />
     </SpeedDial>
